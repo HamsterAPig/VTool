07f8fd512fa982c776816ea5975a85a00a74b7bb
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2026-03-12 21:20:20 +0800

    :zap: perf: 优化全站中文字体资源加载

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,6 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import '@fontsource-variable/manrope/wght.css'
-import '@fontsource/noto-sans-sc/chinese-simplified-400.css'
-import '@fontsource/noto-sans-sc/chinese-simplified-500.css'
-import '@fontsource/noto-sans-sc/chinese-simplified-700.css'
-import '@fontsource/noto-serif-sc/chinese-simplified-600.css'
-import '@fontsource/noto-serif-sc/chinese-simplified-700.css'
 
 import App from '@/app/App.vue'
 import { router, updateDocumentTitle } from '@/router'
