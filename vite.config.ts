caa5f47a767985ace5ff5ece553adde7cb4b1456
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2026-03-17 07:17:48 +0800

    :sparkles: feat(vite): 添加Vue Dev Tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
+/*
+ * @Author: HamsterAPig
+ * @Date: 2026-03-17 07:05:16
+ * @LastEditors: HamsterAPig 102395304+HamsterAPig@users.noreply.github.com
+ * @LastEditTime: 2026-03-17 07:12:04
+ * @FilePath: \VTool\vite.config.ts
+ * @Description: 
+ * Copyright (c) 2026 by HamsterAPig, All Rights Reserved. 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), UnoCSS(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
